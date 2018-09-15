513005b9b7b67a23da0ede3def5af9938ad0c82b
hasan.ali.yuce <hasan.ali.yuce@infonal.com>
2018-09-15 13:06:12 +0300

    controllers & tests

--- src/main/java/com/revolut/bank/exception/TransactionLogValidationException.java
+++
@@ -1,8 +0,0 @@
-package com.revolut.bank.exception;
-
-public class TransactionLogValidationException extends RuntimeException {
-
-    public TransactionLogValidationException(String message) {
-        super(message);
-    }
-}
